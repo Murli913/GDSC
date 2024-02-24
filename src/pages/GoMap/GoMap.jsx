a42528fb90a7f2311379bcbe7cfa98ff86d72562
Hardeep <hardeep0444@gmail.com>
2024-02-24 07:00:20 +0530

    I don't know what changes I made

--- src/pages/GoMap/GoMap.jsx
+++ src/pages/GoMap/GoMap.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const GoMap = () => {
   return (
-    <div className="mb-4">
+    <div className="mb-4 ">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.027953072826!2d77.58476127320962!3d12.970063114915725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae16749c6f9dad%3A0x3f523b887a7b9b!2sBangalore%20Police%20Headquarters!5e0!3m2!1sen!2sin!4v1708706375851!5m2!1sen!2sin"
-        width="2000"
+        width="1700"
         height="150"
         allowFullScreen=""
         loading="lazy"
